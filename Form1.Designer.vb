e2e223f78dcd7dfb2b719c2af8b8b8a8a27d613d
Vilhelm <13785032+SentimentalWoosh@users.noreply.github.com>
2019-07-06 18:14:18 +0200

    First Upload
    Added all the files

---
+++ Form1.Designer.vb
@@ -0,0 +1,236 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CloseButton = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ml_tb = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.g_tb = New System.Windows.Forms.TextBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'CloseButton
+        '
+        Me.CloseButton.AutoSize = True
+        Me.CloseButton.Font = New System.Drawing.Font("Orkney", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.CloseButton.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(110, Byte), Integer), CType(CType(99, Byte), Integer))
+        Me.CloseButton.Location = New System.Drawing.Point(339, 4)
+        Me.CloseButton.Name = "CloseButton"
+        Me.CloseButton.Size = New System.Drawing.Size(19, 18)
+        Me.CloseButton.TabIndex = 0
+        Me.CloseButton.Text = "X"
+        Me.CloseButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Orkney", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(110, Byte), Integer), CType(CType(99, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(8, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(154, 24)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Coffee Helper."
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'ml_tb
+        '
+        Me.ml_tb.BackColor = System.Drawing.Color.FromArgb(CType(CType(78, Byte), Integer), CType(CType(52, Byte), Integer), CType(CType(46, Byte), Integer))
+        Me.ml_tb.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.ml_tb.Font = New System.Drawing.Font("Orkney", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ml_tb.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(110, Byte), Integer), CType(CType(99, Byte), Integer))
+        Me.ml_tb.Location = New System.Drawing.Point(17, 162)
+        Me.ml_tb.Name = "ml_tb"
+        Me.ml_tb.Size = New System.Drawing.Size(329, 20)
+        Me.ml_tb.TabIndex = 2
+        Me.ml_tb.Text = "0"
+        Me.ml_tb.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(110, Byte), Integer), CType(CType(99, Byte), Integer))
+        Me.Panel1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(110, Byte), Integer), CType(CType(99, Byte), Integer))
+        Me.Panel1.Location = New System.Drawing.Point(12, 158)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Padding = New System.Windows.Forms.Padding(1)
+        Me.Panel1.Size = New System.Drawing.Size(338, 28)
+        Me.Panel1.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Orkney", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(110, Byte), Integer), CType(CType(99, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(168, 136)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(28, 19)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "ml"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Orkney", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(110, Byte), Integer), CType(CType(99, Byte), Integer))
+        Me.Label3.Location = New System.Drawing.Point(173, 241)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(19, 19)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "g"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'g_tb
+        '
+        Me.g_tb.BackColor = System.Drawing.Color.FromArgb(CType(CType(78, Byte), Integer), CType(CType(52, Byte), Integer), CType(CType(46, Byte), Integer))
+        Me.g_tb.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.g_tb.Cursor = System.Windows.Forms.Cursors.Default
+        Me.g_tb.Font = New System.Drawing.Font("Orkney", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.g_tb.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(110, Byte), Integer), CType(CType(99, Byte), Integer))
+        Me.g_tb.Location = New System.Drawing.Point(17, 267)
+        Me.g_tb.Name = "g_tb"
+        Me.g_tb.ReadOnly = True
+        Me.g_tb.Size = New System.Drawing.Size(329, 20)
+        Me.g_tb.TabIndex = 5
+        Me.g_tb.Text = "0"
+        Me.g_tb.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(110, Byte), Integer), CType(CType(99, Byte), Integer))
+        Me.Panel2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(110, Byte), Integer), CType(CType(99, Byte), Integer))
+        Me.Panel2.Location = New System.Drawing.Point(12, 263)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Padding = New System.Windows.Forms.Padding(1)
+        Me.Panel2.Size = New System.Drawing.Size(338, 28)
+        Me.Panel2.TabIndex = 6
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Orkney", 8.999999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(110, Byte), Integer), CType(CType(99, Byte), Integer))
+        Me.Button1.Location = New System.Drawing.Point(12, 56)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(92, 24)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Golden Ratio"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Orkney", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(110, Byte), Integer), CType(CType(99, Byte), Integer))
+        Me.Label4.Location = New System.Drawing.Point(13, 136)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(55, 19)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Water"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Orkney", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(110, Byte), Integer), CType(CType(99, Byte), Integer))
+        Me.Label5.Location = New System.Drawing.Point(13, 241)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(58, 19)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Coffee"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button2
+        '
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Orkney", 8.999999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(110, Byte), Integer), CType(CType(99, Byte), Integer))
+        Me.Button2.Location = New System.Drawing.Point(110, 56)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(92, 24)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "Stronger"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Orkney", 8.999999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(110, Byte), Integer), CType(CType(99, Byte), Integer))
+        Me.Button3.Location = New System.Drawing.Point(208, 56)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(92, 24)
+        Me.Button3.TabIndex = 12
+        Me.Button3.Text = "Milder"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(78, Byte), Integer), CType(CType(52, Byte), Integer), CType(CType(46, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(362, 353)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.g_tb)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ml_tb)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CloseButton)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CloseButton As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ml_tb As TextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents g_tb As TextBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,76 @@
+﻿Public Class Form1
+    Private Sub CloseButton_Click(sender As Object, e As EventArgs) Handles CloseButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub CloseButton_MouseHover(sender As Object, e As EventArgs) Handles CloseButton.MouseHover
+        Me.Cursor = Cursors.Hand
+    End Sub
+
+    Private Sub CloseButton_MouseLeave(sender As Object, e As EventArgs) Handles CloseButton.MouseLeave
+        Me.Cursor = Cursors.Default
+    End Sub
+
+    Private Sub CloseButton_MouseEnter(sender As Object, e As EventArgs) Handles CloseButton.MouseEnter
+        Me.Cursor = Cursors.Hand
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Ml_tb_TextChanged(sender As Object, e As EventArgs) Handles ml_tb.TextChanged
+
+        If ml_tb.Text <> "" Then
+            g_tb.Text = Format(Convert.ToInt32(ml_tb.Text) / My.Settings.Ratio, "0.00")
+        End If
+
+    End Sub
+
+    Private Sub Ml_tb_KeyPress(sender As Object, e As KeyPressEventArgs) Handles ml_tb.KeyPress
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub G_tb_KeyPress(sender As Object, e As KeyPressEventArgs) Handles g_tb.KeyPress
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub G_tb_TextChanged(sender As Object, e As EventArgs) Handles g_tb.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        My.Settings.Ratio = 16
+
+        If ml_tb.Text <> "" Then
+            g_tb.Text = Format(Convert.ToInt32(ml_tb.Text) / My.Settings.Ratio, "0.00")
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        My.Settings.Ratio = 14
+
+        If ml_tb.Text <> "" Then
+            g_tb.Text = Format(Convert.ToInt32(ml_tb.Text) / My.Settings.Ratio, "0.00")
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        My.Settings.Ratio = 18
+
+        If ml_tb.Text <> "" Then
+            g_tb.Text = Format(Convert.ToInt32(ml_tb.Text) / My.Settings.Ratio, "0.00")
+        End If
+
+    End Sub
+End Class
